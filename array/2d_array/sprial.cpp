813cff5bc1928ffea079a17cdc6a7e703d51715a
prathamesh-499 <dpratham499@gmail.com>
2025-09-27 23:54:19 +0530

    changed

--- array/2d_array/sprial.cpp
+++ array/2d_array/sprial.cpp
@@ -1,28 +1,34 @@
 #include<iostream>
 using namespace std;
 
-void sprial_print(int arr[][4],int n ,int m){
+void sprial_print(int arr[][3],int n ,int m){
     int srow=0,scol=0,erow=n-1,ecol=m-1;
-    for(int j=scol;j<=ecol;j++){//top
-        cout<<arr[srow][j];
-    }
-    for(int i=srow+1;i<=erow;i++){//right
-        cout<<arr[i][ecol];
-    }
-    for(int j=ecol-1;j>=scol;j--){//bottom 
-        cout<<arr[erow][j];
-    }
-    for(int i=erow-1;i>=srow+1;i--){//left
-        cout<<arr[i][scol];
-    }
-
+    while(srow<=erow && scol<=ecol){// = is for odd case of mid is printed 
+        for(int j=scol;j<=ecol;j++){//top
+        cout<<arr[srow][j]<<' ';
+        }
+        for(int i=srow+1;i<=erow;i++){//right
+            cout<<arr[i][ecol]<<' ';
+        }
+        for(int j=ecol-1;j>=scol;j--){//bottom 
+            if(erow==srow){break;}// for odd case so no duplicted is printed
+            cout<<arr[erow][j]<<' ';
+        }
+        for(int i=erow-1;i>=srow+1;i--){//left
+            if(ecol==scol){break;}// for odd case so no duplicted is printed
+            cout<<arr[i][scol]<<' ';
+        }
+        srow++;scol++;
+        ecol--,erow--;}
 }
 
 int main(){
-    int matrix[4][4]={{ 1, 2, 3, 4},
-                      { 5, 6, 7, 8},
-                      { 9,10,11,12},
-                      {13,14,15,16}};
-    sprial_print(matrix,4,4);
+    int matrix[6][3]={{ 1, 2, 3},
+                      { 5, 6, 7},
+                      { 9,10,11},
+                      {12,13,14},
+                      {12,13,14},
+                      {12,13,14}};
+    sprial_print(matrix,6,3);
     return 0;
 }
